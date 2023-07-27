93cb6a6c5a80d098472e48093f0e310feb4c0285
Mocwiri <88290813+Mocwiri@users.noreply.github.com>
2023-07-27 15:36:08 +0200

    Updated new.py

--- new.py
+++ new.py
@@ -5,3 +5,5 @@ print("First Practice Using notepadd++")
 print("Now lets push you to github")
 
 print("And lets Clone you and see if you work")
+
+print("Now I know how to clone from GitHub using Git)
