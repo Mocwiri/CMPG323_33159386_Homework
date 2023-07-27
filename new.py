2fdc750b866c5122edc86ac1964f942ff3e01db8
Mocwiri <88290813+Mocwiri@users.noreply.github.com>
2023-07-27 16:00:29 +0200

    Update new.py

--- new.py
+++ new.py
@@ -7,3 +7,5 @@ print("Now lets push you to github")
 print("And lets Clone you and see if you work")
 
 print("Now I know how to clone from GitHub using Git")
+
+print("So now I can Pull and Push from github using git")
